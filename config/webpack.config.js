ecd303dac7d014295cbb0079f3205bf36d8f50af
RampantDespair <roguewars@outlook.com>
2023-12-28 19:58:45 -0500

    Updated files to respect eslint rules

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,27 +1,27 @@
-"use strict";
+'use strict'
 
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const paths = {
-  src: path.resolve(__dirname, "../src"),
-  out: path.resolve(__dirname, "../out"),
-};
+  src: path.resolve(__dirname, '../src'),
+  out: path.resolve(__dirname, '../out'),
+}
 
 const config = {
   entry: {
-    popup: paths.src + "/popup.tsx",
-    contentScript: paths.src + "/contentScript.tsx",
-    background: paths.src + "/background.tsx",
-    colors: paths.src + "/colors.tsx",
+    popup: paths.src + '/popup.tsx',
+    contentScript: paths.src + '/contentScript.tsx',
+    background: paths.src + '/background.tsx',
+    colors: paths.src + '/colors.tsx',
   },
   output: {
     path: paths.out,
-    filename: "[name].js",
+    filename: '[name].js',
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   stats: {
     all: false,
     errors: true,
@@ -31,46 +31,46 @@ const config = {
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              outputPath: "images",
-              name: "[name].[ext]",
+              outputPath: 'images',
+              name: '[name].[ext]',
             },
           },
         ],
       },
       {
         test: /\.ts(x)?$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: "**/*",
-          context: "public",
+          from: '**/*',
+          context: 'public',
         },
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: '[name].css',
     }),
   ],
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
   },
-};
+}
 
-module.exports = config;
+module.exports = config
