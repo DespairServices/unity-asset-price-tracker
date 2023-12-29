ec85639849fddb9819cf75699dbf8bf72febd671
RampantDespair <roguewars@outlook.com>
2023-12-28 19:32:13 -0500

    Attempt to fix colors.tsx issues

--- src/colors.tsx
+++ src/colors.tsx
@@ -1,5 +1,5 @@
-export const colorWhite =  '#FFFFFF';
-export const colorGray =   '#AAAAAA';
-export const colorGreen =  '#55FF55';
-export const colorYellow = '#FFFF55';
-export const colorRed =    '#FF5555';
+export const colorWhite =  "#FFFFFF";
+export const colorGray =   "#AAAAAA";
+export const colorGreen =  "#55FF55";
+export const colorYellow = "#FFFF55";
+export const colorRed =    "#FF5555";
